73f0040fb0c3d7f08a8c5aade59a902b5907bf0d
shadow <A257-cloud>
2026-09-15 13:21:05 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+def Addition (nbr1, nbr2):
+    return nbr1 + nbr2
+
+def Soustraction (nbr1, nbr2):
+    return nbr1 - nbr2
+
+def Multiplication (nbr1, nbr2):
+    return nbr1 * nbr2 
+
+def Division (nbr1, nbr2):
+    return nbr1 / nbr2
+
+def menu():
+    print("\n" + "="*30)
+    print("CALCULATRICE :)")
+    print("="*30)
+    print("1. Addition")
+    print("2. Soustraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("0. Quittez")
+    print("="*30)
+
+
+while True:
+    menu()
+    choix = input("votre choix : ").strip()
+
+    if choix == "0":
+        print("\n Merci d'avoir utiliser la calculatrice. Au revoir !")
+    if choix not in ("1", "2", "3", "4"):
+        print(" choix invalide, réessayer")
+
+    nbr1 = float(input("nombre1 >"))
+    nbr2 = float(input("nombre2 >"))
+
+    match choix:
+        case "1":
+            resultat = Addition(nbr1,nbr2)
+        case "2":
+            resultat = Soustraction(nbr1,nbr2)
+        case "3":
+            resultat = Multiplication(nbr1,nbr2)
+        case "4":
+            resultat =  Division(nbr1,nbr2)
+
+    print(f"resultat : {resultat}")
